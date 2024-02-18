00146f95dbcf2bf103955d1f14b4f5bc88ae132f
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-18 19:03:37 +0800

    feat: add function to build tree

--- Tree.js
+++ Tree.js
@@ -8,4 +8,18 @@ export class Tree {
     #removeDupesAndSortArray = (arr) => {
         return [...new Set(arr)].sort((a, b) => a - b);
     }
+
+    buildTree = (arr, start = 0, end = arr.length - 1) => {
+        // Ref: https://www.youtube.com/watch?v=VCTP81Ij-EM
+        if (start > end) return null;
+        let mid = Math.floor((start + end) / 2);
+
+        let rootNode = new Node(
+            arr[mid],
+            this.buildTree(arr, start, mid - 1),
+            this.buildTree(arr, mid + 1, end)
+        )
+
+        return rootNode;
+    }
 } 
