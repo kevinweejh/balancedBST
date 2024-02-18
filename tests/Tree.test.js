a659fd5045a2f94bab1ede2c44751f9cc0e9f599
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-18 21:49:49 +0800

    test: add unit tests for insert function

--- tests/Tree.test.js
+++ tests/Tree.test.js
@@ -1,6 +1,8 @@
 import { Tree } from '../src/Tree.js';
+import { Node } from '../src/Node.js';
 
 describe('Tree class tests', () => {
+    // buildTree()
     test('creates a balanced BST', () => {
         const tree = new Tree([1, 3, 2, 2]);
         
@@ -18,5 +20,36 @@ describe('Tree class tests', () => {
         const tree = new Tree([]);
 
         expect(tree.root).toBeNull();
+    })
+
+    // insert()
+    test('handles insertion of 1 non-repeated number', () => {
+        const tree = new Tree([1, 3, 2, 2]);
+        tree.insert(4);
+
+        expect(tree.root.right.right.key).toBe(4);
+    })
+
+    test('handles insertion of 2 non-repeated numbers', () => {
+        const tree = new Tree([1, 3, 2, 2]);
+        tree.insert(4);
+        tree.insert(0);
+
+        expect(tree.root.right.right.key).toBe(4);
+        expect(tree.root.left.left.key).toBe(0);
+    })
+
+    test('handles insertion of repeated number', () => {
+        const tree = new Tree([1, 3, 2, 2]);
+        tree.insert(3);
+
+        expect(tree.root.right.right).toBeNull();
+    })
+
+    test('handles insertion into empty BST', () => {
+        const tree = new Tree();
+        tree.insert(3);
+
+        expect(tree.root).toEqual(new Node(3, null, null));
     })
 })
