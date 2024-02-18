4149181263ecf6a9fdf9d7bd8056ea7c111ef12a
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-18 18:58:01 +0800

    feat: add private utility function to remove duplicates and sort array before building tree

--- Tree.js
+++ Tree.js
@@ -2,6 +2,10 @@ import { Node } from './Node';
 
 export class Tree {
     constructor(arr = []) {
-        this.root = arr;
+        this.root = this.#removeDupesAndSortArray(arr);
+    }
+
+    #removeDupesAndSortArray = (arr) => {
+        return [...new Set(arr)].sort((a, b) => a - b);
     }
 } 
